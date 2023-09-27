19388e13765e046e008140311a51116c8b64a119
tjwjdals <wjdals030903@g.shingu.ac.kr>
2023-09-27 15:45:50 +0900

    week05 v26

--- week05/src/Pokemon.java
+++ week05/src/Pokemon.java
@@ -7,28 +7,25 @@ public abstract class Pokemon {
     protected int attackRate;
     protected int defenceRate;
 
+    protected String[] skills;  // 3가지 skill 추가 예정
+    protected int[] specialAttackRate;
     private static int pokemonCount = 0;  // 클래스(정적) 변수
 
     Flyable flyable;  // 연관 관계
-
     public void setFlyable(Flyable flyable) {  // upcast
         this.flyable = flyable;
     }
-
     public void performFly(){
         System.out.print(this.name + "이(가) ");
         this.flyable.fly();
     }
-
     public static int getPokemonCount() { // 클래스(정적) 메서드
         return pokemonCount;
     }
-
     public Pokemon() {
         //System.out.println("부모 클래스의 기본 생성자");
         pokemonCount++;
     }
-
     public Pokemon(int level, int hp, String name) {
         //System.out.println("부모 클래스의 매개변수 생성자");
         this.level = level;
@@ -36,23 +33,18 @@ public abstract class Pokemon {
         this.name = name;
         pokemonCount++;
     }
-
     public int getLevel() {
         return level;
     }
-
     public void setLevel(int level) {
         this.level = level;
     }
-
     public int getHp() {
         return hp;
     }
-
     public void setHp(int hp) {
         this.hp = hp;
     }
-
     public void evolve(){  // 매개변수 제거
         if(this instanceof Pikachu){
             System.out.println("삐까삐까~");
@@ -61,7 +53,6 @@ public abstract class Pokemon {
         }else if(this instanceof Charizard){
             System.out.println("자몽자몽~~");
         }
-
         this.level = this.level + 1;  // 매개변수 pokemon 대신에 this 사용
         this.hp = this.hp + 100;  // 레벨 업시 체력 +100
         String texts = """
@@ -71,7 +62,6 @@ public abstract class Pokemon {
                 .replace("name", this.name);
         System.out.println(texts);
     }
-
     public final void info(){  // 자식클래스에서 재정의(override) 불가
         System.out.println("================");
         System.out.println("이름 : " + name);
@@ -79,18 +69,16 @@ public abstract class Pokemon {
         System.out.println("체력 : " + hp);
         System.out.println("================");
     }
-
     public abstract void attack();
-
     public void attack(Pokemon targetPokemon, String skill){
         System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ skill +" 공격 시전!");
         int temporaryAttackRate = this.attackRate - targetPokemon.defenceRate;
-        if (temporaryAttackRate < 0)
+        if(temporaryAttackRate < 0)
             temporaryAttackRate = 0;
         targetPokemon.hp = targetPokemon.hp - temporaryAttackRate;
-        if (targetPokemon.hp <= 0) {
+        if(targetPokemon.hp <= 0){
             System.out.println(targetPokemon.name + "은(는) 사망!");
-        } else {
+        }else{
             System.out.println(targetPokemon.name + "의 체력은 " + targetPokemon.hp + "입니다");
         }
     }
--- week05/src/Squirtle.java
+++ week05/src/Squirtle.java
@@ -4,7 +4,8 @@ public class Squirtle extends Pokemon{
         System.out.println("꼬북꼬북..");
         //this.setLevel(1);
         this.level = 1;
-        this.attackRate = 48;
+        // this.attackRate = 48;   // 미진화 포켓몬의 경우 최대 공격력과 최소 공격력의 차를 3으로 두고 랜덤 추출.
+        this.attackRate = (int)(Math.random() * 4) + 45;  // 445 ~ 48
         this.defenceRate = 65;
         this.setHp(44);
         this.name = "꼬부기";
