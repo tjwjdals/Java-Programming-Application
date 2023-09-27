40e1c1521dab6b8dea7e2f255680c0b54790302f
tjwjdals <wjdals030903@g.shingu.ac.kr>
2023-09-27 14:22:10 +0900

    week05 v24

--- week05/src/Pokemon.java
+++ week05/src/Pokemon.java
@@ -5,6 +5,7 @@ public abstract class Pokemon {
     private int hp;
     protected String name;
     protected int attackRate;
+    protected int defenceRate;
 
     private static int pokemonCount = 0;  // 클래스(정적) 변수
 
@@ -83,8 +84,11 @@ public abstract class Pokemon {
 
     public void attack(Pokemon targetPokemon, String skill){
         System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ skill +" 공격 시전!");
-        targetPokemon.hp = targetPokemon.hp - this.attackRate;
-        //targetPokemon.hp = this.
-        System.out.println(targetPokemon.name + "의 체력은 " + targetPokemon.hp + "입니다");
+        targetPokemon.hp = targetPokemon.hp - targetPokemon.defenceRate;
+        if (targetPokemon.hp <= 0) {
+            System.out.println(targetPokemon.name + "은(는) 사망!");
+        } else {
+            System.out.println(targetPokemon.name + "의 체력은 " + targetPokemon.hp + "입니다");
+        }
     }
 }
