d3e296f6f4e096b5f6d6859fa8c8aa69fe0ef54e
tjwjdals <wjdals030903@g.shingu.ac.kr>
2023-11-01 13:57:29 +0900

    week10 v45

---
+++ week10/src/fly/Wings.java
@@ -0,0 +1,11 @@
+package fly;
+
+import Pokemonster.Flyable;
+
+public class Wings implements Flyable {
+//    @Override
+//    public void fly() {
+//
+//        System.out.println("날개를 펼쳐 날아갑니다~~");
+//    }
+}
