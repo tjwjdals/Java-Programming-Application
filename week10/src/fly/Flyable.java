2f06ffb20cbdc8b96552207d896fd383d111aea0
tjwjdals <wjdals030903@g.shingu.ac.kr>
2023-11-01 15:29:48 +0900

    week10 v48

--- week10/src/fly/Flyable.java
+++ week10/src/fly/Flyable.java
@@ -7,8 +7,8 @@ public interface Flyable {
 
     //Pokemonster.Pokemon pokemon;
 
-    //void fly();
-    default void fly(){
-        System.out.println("날아 갑니다!");
-    }
+    void fly(); // 추상 메서드
+//    default void fly(){
+//        System.out.println("날아 갑니다!");
+//    }
 }
